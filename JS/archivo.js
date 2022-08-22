288b82198dc1f2a082e27a858ddf812d50a15ab1
Dieguitzu <diego.m.escudero@gmail.com>
2022-08-21 21:22:21 -0300

    subida con objeto

--- JS/archivo.js
+++ JS/archivo.js
@@ -1,16 +1,32 @@
-let entrada = prompt("Ingrese su nombre")
+/*Simulador de compra con descunto suma del iva 
+Cosas a mejorar (aparte de todo):
+                                . El descuento es fijo y no un porcentaje
+                                . Lo estoy haciendo sobre un producto solamente
+                                . Estoy seguro que hay formas de simplificar y ordenar mejor el codigo, pero lo hice funcionar 
+                                . Estuve buscando maneras de llamarlo al html creo que no lo vimos todavía*/
 
-while(entrada != "ESC"){
-    switch(entrada){
-        case"lucas":
-            alert("Hola Lucas");
-            break;
-        case"diego":
-            alert("Hola Diego");
-            break;
-        default:
-            alert("¿QUIEN SO'?")
-            break;
+
+
+    const suma = (a,b) => a + b;
+const resta = (a,b) => a - b;
+const multip = (a,b) => a * b;
+
+class jabon{
+    constructor(nombre, precio, tamaño){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.tamaño = tamaño;
     }
-    entrada= prompt("ingrese su nombre")
 }
+let jabon1 = new jabon("Carbon activado",350,"100gr")
+
+const iva = x => x * 0.21
+let cantidad = prompt("Ingrese cantidad")
+let precio2 = multip(cantidad, jabon1.precio)
+const descuento = multip(cantidad,50) 
+let total = resta(suma(precio2, iva (precio2)),descuento);
+
+alert("Precio final $" + total);
+
+jabon1 = document.getElementById('ul');
+jabon1.insertAdjacentHTML('afterend',"$" + total);
