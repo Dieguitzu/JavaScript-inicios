b982d3ac1bffc42d693a64e74dd4fe0b8c38cc28
Dieguitzu <diego.m.escudero@gmail.com>
2022-08-30 16:43:35 -0300

    Arrays

--- JS/archivo.js
+++ JS/archivo.js
@@ -1,32 +1,40 @@
-/*Simulador de compra con descunto suma del iva 
-Cosas a mejorar (aparte de todo):
-                                . El descuento es fijo y no un porcentaje
-                                . Lo estoy haciendo sobre un producto solamente
-                                . Estoy seguro que hay formas de simplificar y ordenar mejor el codigo, pero lo hice funcionar 
-                                . Estuve buscando maneras de llamarlo al html creo que no lo vimos todavía*/
-
-
-
-    const suma = (a,b) => a + b;
+const suma = (a,b) => a + b;
 const resta = (a,b) => a - b;
 const multip = (a,b) => a * b;
 
-class jabon{
+class Producto{
     constructor(nombre, precio, tamaño){
-        this.nombre = nombre;
-        this.precio = precio;
-        this.tamaño = tamaño;
+        this.nombre = nombre
+        this.precio = precio
+        this.tamaño = tamaño
     }
 }
-let jabon1 = new jabon("Carbon activado",350,"100gr")
 
-const iva = x => x * 0.21
-let cantidad = prompt("Ingrese cantidad")
-let precio2 = multip(cantidad, jabon1.precio)
-const descuento = multip(cantidad,50) 
-let total = resta(suma(precio2, iva (precio2)),descuento);
+const productos = [];
+productos.push(new Producto("0. Jabon de carbon activado",350,"100gr"));
+productos.push(new Producto("1. Jabon de Lavanada",350,"100gr"));
+productos.push(new Producto ("2. Jabon de Canela y curcuma",350,"100gr"));
+productos.push(new Producto("3. Jabon Neutro",350, "100gr"));
+productos.push(new Producto("4. Shampoo solido",600,"100gr"));
+productos.push(new Producto("5. Acondicionador solido",600,"100gr"));
+productos.push(new Producto("6. Cepillo de dientes de Bamboo",400,"adulto"));
+productos.push(new Producto("7. Hilo dental",500,"30Mtrs"));
 
-alert("Precio final $" + total);
+//Lista de productos
 
-jabon1 = document.getElementById('ul');
-jabon1.insertAdjacentHTML('afterend',"$" + total);
+let seleccion = prompt ("¿Desea comprar alguno de nuestro productos si o no?")
+let seleccion2 = 0;
+
+while(seleccion != "si" && seleccion != "no"){
+    alert("continuar con si o con no")
+    seleccion = prompt("¿Hola, desea comprar algo si o no?")
+    }
+    if (seleccion == "si"){
+        alert("Acontinuación, presione el numero del producto que desee!")
+        let todosLosProductos = productos.map(
+            (producto) => producto.nombre + " $" + producto.precio);
+        alert(seleccion2 = prompt(todosLosProductos.join("\n")));
+            console.log(productos[seleccion2]);
+    } else if(seleccion == "no"){
+        alert ("Gracias por visitarnos, hasta pronto!")
+    }
